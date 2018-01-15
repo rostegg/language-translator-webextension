34da29a11f9c1e826bd2cef1be3154e5644a0ec9
Rostislav.Nikolaenko <rostik.nikolaenko@gmail.com>
2018-01-15 17:47:32 +0200

    drowpdown menu added

--- panel-module/src/main/kotlin/helper.kt
+++ panel-module/src/main/kotlin/helper.kt
@@ -8,6 +8,7 @@ data class YandexResponse(val code:String, val lang:String, val text: String)
 
 /* here is the key from yandex translate api*/
 
+const val YANDEX_API_KEY = "YOUR_API_KEY"
 
 object Endpoints{
     val TRANSLATE_TEXT = ""
